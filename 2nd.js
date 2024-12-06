56f61fe0b7dda763efcf867b0d51b442d5f1048b
TEJESHWAR M <makkalateja@gmail.com>
2024-12-06 12:38:24 +0530

    Delete 2nd.js

--- 2nd.js
+++
@@ -1,9 +0,0 @@
-Script.js
-document.querySelectorAll('.profile').forEach(profile => {
-    profile.addEventListener('mouseenter', () => {
-      profile.style.backgroundColor = '#f0f0f0';
-    });
-    profile.addEventListener('mouseleave', () => {
-      profile.style.backgroundColor = 'black';
-    });
-  });
